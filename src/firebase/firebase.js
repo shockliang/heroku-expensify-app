20b5f1cd77f1eedef90c34568520fbb5de558d54
shockliang <shockwater@gmail.com>
2019-05-25 00:32:45 +0800

    Updating data to firebase.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,33 +14,52 @@ firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
 
-// database
-//   .ref()
-//   .set({
-//     name: "Shock",
-//     age: 30,
-//     isSingle: false,
-//     location: {
-//       city: "Taichung",
-//       country: "Taiwan"
-//     }
-//   })
-//   .then(() => {
-//     console.log("Data is saved");
-//   })
-//   .catch(e => {
-//     console.log("This failed.", e);
-//   });
-
-database.ref('isSingle').set(null);
-
 database
-  // .ref("isSingle")
   .ref()
-  .remove()
+  .set({
+    name: "Shock",
+    age: 30,
+    stressLevel: 6,
+    job: {
+      title: "Software developer",
+      company: "SGT"
+    },
+    isSingle: false,
+    location: {
+      city: "Taichung",
+      country: "Taiwan"
+    }
+  })
   .then(() => {
-    console.log("data was removed");
+    console.log("Data is saved");
   })
   .catch(e => {
-    console.log("Did not remove data", e);
+    console.log("This failed.", e);
   });
+
+database
+  .ref()
+  .update({
+    stressLevel: 9,
+    "job/company": "Amazon",
+    "location/city": "Seattle"
+  })
+  .then(() => {
+    console.log("data was updated");
+  })
+  .catch(e => {
+    console.log("Did not update data", e);
+  });
+
+// database.ref('isSingle').set(null);
+
+// database
+//   // .ref("isSingle")
+//   .ref()
+//   .remove()
+//   .then(() => {
+//     console.log("data was removed");
+//   })
+//   .catch(e => {
+//     console.log("Did not remove data", e);
+//   });
