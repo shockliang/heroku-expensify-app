28a4fe28b4b12c37a81f7669629075b5cdcfc95e
shockliang <shockwater@gmail.com>
2019-06-07 00:05:48 +0800

    Public only routes.

--- src/routers/PublicRoute.js
+++ src/routers/PublicRoute.js
@@ -3,23 +3,16 @@ import { connect } from "react-redux";
 import { Route, Redirect } from "react-router-dom";
 import Header from "../components/Header";
 
-export const PrivateRoute = ({
+export const PublicRoute = ({
   isAuthenticated,
   component: Component,
   ...rest
 }) => (
   <Route
     {...rest}
-    component={props => (
-      isAuthenticated ? (
-        <div>
-          <Header />
-          <Component {...props} />
-        </div>
-      ) : (
-        <Redirect to="/" />
-      )
-    )}
+    component={props =>
+      isAuthenticated ? <Redirect to="/dashboard" /> : <Component {...Props} />
+    }
   />
 );
 
@@ -27,6 +20,4 @@ const mapStateToProps = state => ({
   isAuthenticated: !!state.auth.uid
 });
 
-export default connect(mapStateToProps)(PrivateRoute);
-
-
+export default connect(mapStateToProps)(PublicRoute);
