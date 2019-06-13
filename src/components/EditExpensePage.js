8b19e4c79645cb26554c2d68874fec5b5c967a96
shockliang <shockwater@gmail.com>
2019-06-14 00:09:53 +0800

    Styling expense form.

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,10 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import ExpenseForm from "./ExpenseForm";
-import {
-  startEditExpense,
-  startRemoveExpense
-} from "../actions/expenses";
+import { startEditExpense, startRemoveExpense } from "../actions/expenses";
 
 export class EditExpensePage extends React.Component {
   editExpense = expense => {
@@ -20,8 +17,18 @@ export class EditExpensePage extends React.Component {
   render() {
     return (
       <div>
-        <ExpenseForm expense={this.props.expense} onSubmit={this.editExpense} />
-        <button onClick={this.removeExpense}>Remove</button>
+        <div className="page-header">
+          <div className="content-container">
+            <h1 className="page-header__title">Edit Expense</h1>
+          </div>
+        </div>
+        <div className="content-container">
+          <ExpenseForm
+            expense={this.props.expense}
+            onSubmit={this.editExpense}
+          />
+          <button className="button--secondary" onClick={this.removeExpense}>Remove Expense</button>
+        </div>
       </div>
     );
   }
