cc9d969cb92b03294a9d9d4da54e0fc02eebfc10
shockliang <shockwater@gmail.com>
2019-06-15 00:26:53 +0800

    Styling expenses list item.

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,17 +4,19 @@ import ExpenseListItem from "./ExpenseListItem";
 import selectExpense from "../selectors/expenses";
 
 export const ExpenseList = props => (
-  <div>
-    <h1>Expense List</h1>
-    {
-      props.expenses.length === 0 ? (
-        <p>No expenses</p>
-      ) : (
-        props.expenses.map(expense => {
-          return <ExpenseListItem key={expense.id} {...expense} />;
-        })
-      )
-    }
+  <div className="content-container">
+    <div className="list-header">
+      <div className="show-for-mobile">Expenses</div>
+      <div className="show-for-desktop">Expense</div>
+      <div className="show-for-desktop">Amount</div>
+    </div>
+    {props.expenses.length === 0 ? (
+      <p>No expenses</p>
+    ) : (
+      props.expenses.map(expense => {
+        return <ExpenseListItem key={expense.id} {...expense} />;
+      })
+    )}
   </div>
 );
 
